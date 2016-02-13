8c43319e5f4d4293d60448a6bbb36fc365b6450d
Justin Noah <justin.noah@afamilyforeverychild.org>
2016-02-13 09:14:20 -0800

    models: cleanup

--- app/models.py
+++ app/models.py
@@ -1,39 +1,45 @@
-from sqlalchemy import Column, DateTime, Integer, String, Boolean, Text, \
-    ForeignKey, or_, desc
+"""Test data tables."""
 
-from __init__ import db, bcrypt
-"""
-test data tables
-"""
+from sqlalchemy import (
+    Column, DateTime, Integer, String, Boolean, Text, ForeignKey, or_, desc
+)
+
+from . import db, bcrypt
+
+
 class User(db.Model):
+    """Basic user model."""
 
-	__tablename__ = "users"
-	id = db.Column(db.Integer, primary_key=True)
-	email = db.Column(db.String(50), nullable=False)
-	password = db.Column(db.String(20), nullable=False)
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
 
-	def __init__(self, email, password):
-		self.email = email
-		self.password = bcrypt.generate_password_hash(password)
+    def __init__(self, email, password):
+        """Initialize the user with a required email address and password."""
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
 
-	def __repr__(self):
-		return '<{}>'.format(self.email)
+    def __repr__(self):
+        """Return <%(email)."""
+        return '<{}>'.format(self.email)
 
-	@property 
-	def is_authenticated(self):
-		return True
+    @property
+    def is_authenticated(self):
+        """Athentication check."""
+        return True
 
-	@property 
-	def is_active(self):
-		return True
+    @property
+    def is_active(self):
+        """Active check."""
+        return True
 
-	@property 
-	def is_anonymous(self):
-		return True
+    @property
+    def is_anonymous(self):
+        """Anonimity check."""
+        return True
 
-	@property
-	def get_id(self):
-		return str(self.id)
-
-	def __repr__(self):
-		return '<User %r>' % (self.name)
+    @property
+    def get_id(self):
+        """User ID getter."""
+        return str(self.id)

8aa2ef80a6dc1790c69fca1d86fe6d17e1746ab2
Justin Noah <justin.noah@afamilyforeverychild.org>
2016-02-13 09:13:26 -0800

    forms: cleanup

--- app/forms.py
+++ app/forms.py
@@ -4,23 +4,28 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, Required
 
 
 class RegisterForm(Form):
-	"""
-	Creates a form that requires an email, 
-	password, phone number, and checked boxes.
+    """
+    Registration Form.
 
-	"""
-	email = TextField(
-		'email',
-		validators=[DataRequired(), Email(message=None), Length(min=6, max=40)]
-		)
-	phone_number = TextField(
-		'number',
-		validators=[DataRequired(), Length(min=10)])
-	password = PasswordField(
+    Required fields are:
+        email, password, phone number, and checked boxes.
+    """
+
+    email = TextField(
+        'email',
+        validators=[DataRequired(), Email(message=None), Length(min=6, max=40)]
+        )
+    phone_number = TextField(
+        'number',
+        validators=[DataRequired(), Length(min=10)])
+    password = PasswordField(
         'password',
         validators=[DataRequired(), Length(min=2, max=25)]
     )
-	confirm = PasswordField(
-		'Repeat password',
-		validators=[DataRequired(), EqualTo('password', message='Passwords muct match.')]
-		)
+    confirm = PasswordField(
+        'Repeat password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords muct match.')
+        ]
+    )

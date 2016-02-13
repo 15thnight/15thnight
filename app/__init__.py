124591e8c81aa1d878dc7ece018aa364ee065468
joseph Livni <j.d.livni@gmail.com>
2016-02-13 09:09:39 -0800

    Registration

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from functools import wraps
 from flask.ext.login import login_user, logout_user, current_user, login_required, LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt 
+from functools import wraps
+import gc
 
 app = Flask('15thnight')
 bcrypt = Bcrypt(app)
@@ -11,31 +13,109 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///testing.db'
 app.secret_key = 'my precious'
 db = SQLAlchemy(app)
 from forms import RegisterForm
+from forms import LoginForm
+from forms import AlertForm
 from models import *
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/')
 def index():
-    return 'hello'
+    return render_template('base.html')
 
 @app.route('/register', methods=['GET','POST'])
 def register():
 	form = RegisterForm()
+	data = ['Shelter','Clothes','Food','Other']
 	if request.method == 'POST' and form.validate():
+		selected_users = request.form.getlist("tags")
 		user = User(
 			email=form.email.data,
 			password=form.password.data,
-			phone_number=form.phone_number.data)
-
+			phone_number=form.phone_number.data,
+			other=form.other.data,
+			shelter=form.shelter.data,
+			food=form.food.data,
+			clothes=form.clothes.data
+			)
+		
 		db.session.add(user)
 		db.session.commit()
+		
 		session['logged_in'] = True
+		login_user(user)
+		flash("You have registered!")
+		return redirect(url_for('dashboard'))
+	return render_template('register.html', form=form, data=data)
+
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    form = LoginForm(request.form) #creates instance of form
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=request.form['email']).first()
+            if user is not None and bcrypt.check_password_hash(user.password, request.form['password']):
+                session['logged_in'] = True #session cookie in browser
+                login_user(user)
+                flash('You were logged in.')
+                return redirect(url_for('dashboard'))
+                error = 'Invalid Credentials. Please try again.'
+            else:
+                error = 'Invalid Credentials. Please try again.'
+    return render_template('login.html',form=form, error=error)
+
+
+@login_required
+@app.route('/dashboard', methods=['GET','POST'])
+def dashboard():
+	#alerts = db.session.query(Alert).all()
+	user = User.query.filter_by(id=current_user.id).first()
+	alerts = user.alerts.all()
+	form = AlertForm()
+	if request.method == 'POST' and form.validate_on_submit():
+		alert = Alert(
+			description=form.description.data,
+			other=form.other.data,
+			shelter=form.shelter.data,
+			food=form.food.data,
+			clothes=form.clothes.data,
+			user_id= current_user.id #user id
+			)
+		db.session.add(alert)
+		db.session.commit()
+	
+	return render_template('dashboard.html',form=form, alerts=alerts)
+@login_required
+@app.route("/logout")
+def logout():
+	session.clear()
+	gc.collect()
+	flash('You have been logged out!')
+	return redirect(url_for('register'))
+
+@login_manager.user_loader #required for flask login
+def load_user(id):
+    return User.query.get(int(id))
+
 
 @app.route('/health')
 def healthcheck():
     return 'ok', 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, DateTime, Integer, String, Boolean, Text, \
     ForeignKey, or_, desc
 
 from __init__ import db, bcrypt
+from sqlalchemy.orm import relationship
 """
 test data tables
 """
@@ -11,12 +12,24 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(50), nullable=False)
 	password = db.Column(db.String(20), nullable=False)
-
-	def __init__(self, email, password):
+	phone_number = db.Column(db.String(20), nullable=False)
+	shelter = db.Column(db.String(50), nullable=False, default='')
+	clothes = db.Column(db.String(50), nullable=False, default='')
+	food = db.Column(db.String(50), nullable=False, default='')
+	other = db.Column(db.String(50), nullable=False, default='')
+	alerts = relationship("Alert", backref='user', lazy='dynamic')
+	
+	def __init__(self, email, password, phone_number, other, food, clothes, shelter):
 		self.email = email
 		self.password = bcrypt.generate_password_hash(password)
+		self.phone_number = phone_number
+		self.other = other
+		self.shelter = shelter
+		self.clothes = clothes
+		self.food = food
 
 	def __repr__(self):
+
 		return '<{}>'.format(self.email)
 
 	@property 
@@ -31,9 +44,22 @@ class User(db.Model):
 	def is_anonymous(self):
 		return True
 
-	@property
 	def get_id(self):
-		return str(self.id)
+		try:
+			return unicode(self.id)
+		except NameError:
+			return str(self.id)
 
 	def __repr__(self):
-		return '<User %r>' % (self.name)
+		return '<User %r>' % (self.id)
+
+class Alert(db.Model):
+
+	__tablename__ = 'alerts'
+	id = db.Column(db.Integer, primary_key=True)
+	description = db.Column(db.String(200), nullable=False)
+	shelter = db.Column(db.String(50), nullable=False, default='')
+	clothes = db.Column(db.String(50), nullable=False, default='')
+	food = db.Column(db.String(50), nullable=False, default='')
+	other = db.Column(db.String(50), nullable=False, default='')
+	user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)

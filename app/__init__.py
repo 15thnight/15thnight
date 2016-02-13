e1b9f41e0fb7e008ad2266180ca4e501b01d037c
joseph Livni <j.d.livni@gmail.com>
2016-02-12 23:58:35 -0800

    initial user stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,37 @@
-from flask import Flask
-
+from flask import Flask, render_template, g, redirect, url_for, request, session, flash, g
+from functools import wraps
+from flask.ext.login import login_user, logout_user, current_user, login_required, LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.bcrypt import Bcrypt 
 
 app = Flask('15thnight')
+bcrypt = Bcrypt(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
+app.secret_key = 'my precious'
+db = SQLAlchemy(app)
+from forms import RegisterForm
+from models import *
 @app.route('/')
 def index():
-    return 'web app'
+    return 'hello'
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+	form = RegisterForm()
+	if request.method == 'POST' and form.validate():
+		user = User(
+			email=form.email.data,
+			password=form.password.data,
+			phone_number=form.phone_number.data)
+
+		db.session.add(user)
+		db.session.commit()
+		session['logged_in'] = True
 
 @app.route('/health')
 def healthcheck():
--- app/models.py
+++ app/models.py
@@ -1,2 +1,39 @@
 from sqlalchemy import Column, DateTime, Integer, String, Boolean, Text, \
     ForeignKey, or_, desc
+
+from __init__ import db, bcrypt
+"""
+test data tables
+"""
+class User(db.Model):
+
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(50), nullable=False)
+	password = db.Column(db.String(20), nullable=False)
+
+	def __init__(self, email, password):
+		self.email = email
+		self.password = bcrypt.generate_password_hash(password)
+
+	def __repr__(self):
+		return '<{}>'.format(self.email)
+
+	@property 
+	def is_authenticated(self):
+		return True
+
+	@property 
+	def is_active(self):
+		return True
+
+	@property 
+	def is_anonymous(self):
+		return True
+
+	@property
+	def get_id(self):
+		return str(self.id)
+
+	def __repr__(self):
+		return '<User %r>' % (self.name)
